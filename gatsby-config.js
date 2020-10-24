6555c55a8e860216ed4ef6fed0cfea7f543310ca
Derek Vance <derekvance21@gmail.com>
2020-10-23 19:29:03 -0700

    Initial commit to remote

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,7 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Kent Vincent Photo`,
+    description: `Showing the best of Kent Vincent's amateur photography`,
+  },
   plugins: [],
 }
